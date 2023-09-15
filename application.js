a2a6f584d11d60bf98c33d032d22e75c6b74f4b1
Nathan Peters <peters.nate8@gmail.com>
2023-09-15 17:35:18 -0600

    delete task when remove button clicked

--- application.js
+++ application.js
@@ -11,7 +11,7 @@ var displayTasks = function() {
             response.tasks.forEach(function(task) {
                 console.log(task.content);
                 //inject into DOM
-                var htmlString = '<div class="task d-flex justify-content-evenly align-items-center border-bottom p-2"><span class="remove mt-1"><i class="fa-solid fa-x fa-md" style="color: #e23838;"></i></span><h3 class="text-lowercase mb-0 col-3">' + task.content + '</h3><input type="checkbox" name="complete" id="markComplete" class="form-check form-check-input btn btn-outline-primary"></div>';
+                var htmlString = '<div class="task d-flex justify-content-evenly align-items-center border-bottom p-2"><span class="remove mt-1" data-id="' + task.id + '"><i class="fa-solid fa-x fa-md" style="color: #e23838;"></i></span><h3 class="text-lowercase mb-0 col-3">' + task.content + '</h3><input type="checkbox" name="complete" id="markComplete" class="form-check form-check-input btn btn-outline-primary"></div>';
 
                 $("#todo-list").append(htmlString);
             });
@@ -45,6 +45,20 @@ var createNewTask = function() {
     });   
 }
 
+var deleteTask = function(id) {
+    $.ajax({
+        type: 'DELETE',
+         url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=317',
+         success: function (response, textStatus) {
+           console.log(response);
+           displayTasks();
+         },
+         error: function (request, textStatus, errorMessage) {
+           console.log(errorMessage);
+         }
+    });    
+}
+
 // check if dom is ready
 $(document).ready(function() {
     displayTasks();
@@ -53,5 +67,11 @@ $(document).ready(function() {
     $("#addTask").on('click', function(event) {
         event.preventDefault();
         createNewTask();
+    })
+
+    // delete task when remove button clicked
+    $(document).on('click', '.remove', function() {
+        console.log($(this).data('id'));
+        deleteTask($(this).data('id'));
     })
 });
